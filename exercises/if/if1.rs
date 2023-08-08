fc2738f0e490a5703363787c519561a814fa4749
dreamin2301 <obscurecdq@gmail.com>
2023-08-08 12:42:23 +0800

    lab修改0808

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -9,6 +9,13 @@ pub fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a>b{
+        return a;
+    }
+    else{
+        return b;
+    }
+
 }
 
 // Don't mind this for now :)
--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -3,11 +3,11 @@
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let number = "T-H-R-E-E"; // don't change this line
+    let mut number = "T-H-R-E-E";
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    number = "3"; // 将数字3改为字符串"3"
+    let parsed_number:i32= number.parse().unwrap(); // 字符串转换为数字
+    println!("Number plus two is : {}", parsed_number + 2);
 }
